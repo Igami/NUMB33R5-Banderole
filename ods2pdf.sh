15810b6116d46c404b31da2bdfd087711efe2668
Michael Prange <mpe@vagariT410>
2018-09-22 09:00:19 +0200

    name output pdf

--- ods2pdf.sh
+++ ods2pdf.sh
@@ -4,16 +4,24 @@ input=${1:-Banderole}
 front=.$input.front.pdf
 back=.$input.back.pdf
 
-libreoffice --convert-to csv $input.ods
+libreoffice --convert-to csv --infilter='CSV:44,,76,1,,1031,true' $input.ods
 libreoffice --convert-to pdf $input.ods
 while [ ! -f $input.pdf ]; do sleep 0.1; done
 
-count=$(head -n 5 $input.csv | tail -1 | cut -d, -f11 -)
+count=$(head -n 6 $input.csv | tail -1 | cut -d, -f11 -)
+output="./output/"
+output+=$(head -n 6 $input.csv | tail -1 | cut -d, -f6 -)
+output+=$(head -n 6 $input.csv | tail -1 | cut -d, -f7 -)
+output+=$(head -n 6 $input.csv | tail -1 | cut -d, -f8 -)
+output+="_"
+output+=$(head -n 6 $input.csv | tail -1 | cut -d, -f9 -)
+output+="_"
+output+=$(head -n 3 $input.csv | tail -1 | cut -d, -f2 -)
 
 for ((i=1;i<=$count;i+=9)); do
   max=$(($i+8))
   blanks=1
-  if (("$max" > "$count")); then
+  if ((max > count)); then
     blanks=$((max-count+blanks))
     max=$count
   fi
@@ -32,8 +40,6 @@ pdfjam -q --nup 1x11 --outfile $front
 echo "assemble back"
 pdftk $input.pdf cat $cat_back output - | \
 pdfjam -q --nup 1x11 --outfile $back
-
-output="./output/"$(head -n 2 $input.csv | tail -1 | cut -d, -f4 -)
 
 echo "shuffle pdf"
 pdftk A=$front B=$back shuffle A B output $output.pdf
